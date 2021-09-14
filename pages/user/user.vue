2e365548098b2f873106e6a30f07e21b4a971edb
mos-bahu <2237144586@qq.com>
2021-09-14 09:04:40 +0800

    一些内容的更改

---
+++ pages/user/user.vue
@@ -0,0 +1,25 @@
+<template>
+	<view class="user">
+		<view class="user-image">
+			<image src="../../static/logo.png" mode=""></image>
+			<text>霸虎</text>
+		</view>
+		<view class="user-nubmer">
+			<view v-for="(item,index) in ['关注','粉丝','获赞']" :key="index">
+				<view class="title">
+					1
+				</view>
+				<view class="fan-number">
+					{{item}}
+				</view>
+			</view>
+		</view>
+	    <view></view>
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
